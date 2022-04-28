7997e90f879fb059246c17cfa510f0e8f62e34d3
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 16:07:56 -0700

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" }
+    };
+  },
+  assetPrefix: '.',
+  images: { loader: 'custom' },
 }
+
 
 module.exports = {
   // https://github.com/vercel/next.js/issues/21079
