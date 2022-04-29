f78043b0f87b5cc25bf9cbf9e59690aedadc4a50
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 18:33:27 -0700

    testing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "./" }
+    };
+  },
+  PUBLIC_URL: "/",
+  assetPrefix: './',
+  images: { loader: 'custom' },
 }
+
 
 module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   nextConfig,
+  env: {
+    PUBLIC_URL: "https://your-organization-or-username.github.io/my-nextjs-app",
+    assetPrefix: './'
+  },
   images: {
     loader: 'imgix',
     path: '/',
