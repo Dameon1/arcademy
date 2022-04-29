cd52c25e9dc85169cdfce30a1c4b679b5218e03a
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 20:34:14 -0700

    passing travis

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" }
-    };
-  },
-  assetPrefix: '.',
-  images: { loader: 'custom' },
+  reactStrictMode: false,
 }
 
 module.exports = {
