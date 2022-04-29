2091a1d855763b2546670bb2f847f88a63c2638c
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 22:26:32 -0700

    changed next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+
   exportPathMap: function () {
     return {
       "/": { page: "/" }
     };
   },
-  assetPrefix: '.',
+
   images: { loader: 'custom' },
 }
 
@@ -15,6 +18,8 @@ module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   nextConfig,
+  basePath = isProd ? '/arcademy' : '',
+  assetPrefix = isProd ? '/arcademy' : '',
   images: {
     loader: 'imgix',
     path: '/',
