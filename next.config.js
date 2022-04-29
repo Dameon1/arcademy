77e4afa3f008730d7dab7ea10ff59fd6814b1a0d
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 21:06:26 -0700

    changed next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // reactStrictMode: true,
-  trailingSlash: true,
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" }
-    };
-  },
-  assetPrefix: '.',
-  images: { loader: 'custom' },
 }
 
 module.exports = {
