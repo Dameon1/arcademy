251897577f1ab15c7f3b1b4df3100258bd27bcbc
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-28 18:36:42 -0700

    testing

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  exportPathMap: function () {
-    return {
-      "/": { page: "./" }
-    };
-  },
-  PUBLIC_URL: "/",
-  assetPrefix: './',
-  images: { loader: 'custom' },
+  reactStrictMode: false,
 }
-
 
 module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   nextConfig,
-  env: {
-    PUBLIC_URL: "https://www.dameonmendoza.com/arcademy",
-    assetPrefix: './'
-  },
   images: {
     loader: 'imgix',
     path: '/',
